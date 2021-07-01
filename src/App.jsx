440e387aed236f72ebd6c8b86299a7eea3d18538
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2021-07-01 10:09:01 +0400

    Working app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,44 @@
 import React from 'react';
-import './App.css';
+import { IconButton } from '@chakra-ui/button';
+import { useColorMode } from '@chakra-ui/color-mode';
+import { Flex, VStack, Heading, Spacer } from '@chakra-ui/layout';
+import {
+  FaSun,
+  FaMoon,
+  FaInstagram,
+  FaGithub,
+  FaLinkedin,
+} from 'react-icons/fa';
+import Header from './components/Header';
+import Profile from './components/Profile';
+import Social from './components/Social';
 
 function App() {
-  return <div className='App'>Hello World</div>;
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === 'dark';
+
+  return (
+    <VStack p={5}>
+      <Flex w='100%'>
+        <Heading ml='8' size='md' fontWeight='semibold' color='cyan.400'>
+          Nickname
+        </Heading>
+
+        <Spacer></Spacer>
+        <IconButton icon={<FaLinkedin />} isRound='true'></IconButton>
+        <IconButton ml={2} icon={<FaInstagram />} isRound='true'></IconButton>
+        <IconButton ml={2} icon={<FaGithub />} isRound='true'></IconButton>
+        <IconButton
+          ml={8}
+          icon={isDark ? <FaSun /> : <FaMoon />}
+          isRound='true'
+          onClick={toggleColorMode}></IconButton>
+      </Flex>
+      <Header></Header>
+      <Social></Social>
+      <Profile></Profile>
+    </VStack>
+  );
 }
 
 export default App;
